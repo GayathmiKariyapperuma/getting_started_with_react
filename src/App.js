68365f840568456349df2e9d50acafdedf2aa6f8
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-09 17:36:52 +0530

    react fragment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import "./styles.css";
 
 function App() {
   return (
-    <div>
+    <>
+      {/* react fragment */}
       <h1>Hello</h1>
-    </div>
+      <span>
+        ,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum
+      </span>
+    </>
   );
 }
 
