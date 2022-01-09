b34186b5cf17a37676688d240178d2cd0f005c85
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-09 17:29:04 +0530

    created function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./styles.css";
 
-export default function App() {
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
+
+/*const App = () => {
   return (
     <div className="App">
       <h1>DevTown</h1>
     </div>
   );
-}
-
-//export default App;
+};*/
