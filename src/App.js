846e782e77e943bf214215d090d7cc9af81950eb
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-09 17:38:20 +0530

    create const name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 
 function App() {
+  const name = "Devtown";
+
   return (
     <>
       {/* react fragment */}
       <h1>Hello</h1>
       <span>
-        ,Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
