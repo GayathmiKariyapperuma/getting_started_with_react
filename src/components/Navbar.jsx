1fefc1f91b078309c07c6987e654c4fe5a1d2ce7
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-10 17:50:59 +0530

    edit Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navbar.css";
+import { AiFillAlipayCircle } from "react-icons/ai";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <AiFillAlipayCircle />
+      <div className="links">
+        <button>Home</button>
+        <button>Contact</button>
+        <button>Get Help</button>
+      </div>
+    </div>
+  );
+}
