6f28d42cf280dae0599e5fc58dd70320847ee010
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-10 17:38:33 +0530

    add icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { AiFillAlert } from "react-icons/ai";
 
 function Navigation() {
-  return <h2>Navigation</h2>;
+  return (
+    <div>
+      <h2>Navigation</h2>
+      <AiFillAlert />
+    </div>
+  );
 }
 export default Navigation;
