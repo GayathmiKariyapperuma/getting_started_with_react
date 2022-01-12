ededc8634b6de0572047c28b06c59e82f8d1107b
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-12 17:48:17 +0530

    edit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Home() {
-  return (
-    <>
-      <h1>Getting Started with React</h1>
-    <>
-  );
+  return <h1>Getting Started with React</h1>;
 }
 
 export default Home;
