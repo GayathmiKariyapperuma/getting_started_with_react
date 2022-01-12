dac38094e1da354c4e38d8663c029f62379f7e55
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-12 17:45:11 +0530

    edit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="container">
+    <div className="mainContainer">
       <h1>Getting Started with React</h1>
     </div>
   );
