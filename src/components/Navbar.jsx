5c31befdfd18b55c500efe93c437ed3fd546af84
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-12 17:50:16 +0530

    add links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./navbar.css";
 import { AiFillAlipayCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar">
       <AiFillAlipayCircle className="companyLogo" />
       <div className="links">
-        <button>Home</button>
-        <button>Contact</button>
-        <button>Get Help</button>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/get-help">Get Help</Link>
       </div>
     </div>
   );
