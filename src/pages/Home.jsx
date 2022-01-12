83f250bae50c926503a3df2d196536f3f4a9f4a1
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-12 23:54:52 +0530

    add text to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Home() {
-  return <h1>Getting Started with React</h1>;
+  return (
+    <>
+      <h1>Getting Started with React</h1>;
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        voluptatibus eligendi assumenda culpa odio maxime veritatis aperiam.
+        Cupiditate, enim, provident eaque officiis iusto eligendi, modi unde
+        voluptatum officia asperiores incidunt.
+      </p>
+    </>
+  );
 }
 
 export default Home;
