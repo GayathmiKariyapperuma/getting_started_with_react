3125853007b42206458e0d07a8cdc4ad71a2183f
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-13 00:00:22 +0530

    correct mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-import { Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 // components
 import Navbar from "./components/Navbar";
