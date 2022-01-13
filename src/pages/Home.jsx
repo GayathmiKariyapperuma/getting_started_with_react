de29e47228e6c223a321d06b6868ab1e5b8e157b
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-13 17:31:46 +0530

    add toggle to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 
 function Home() {
-  const [number, setNumber] = useState(0);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <>
       <h1>Getting Started with React</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-        voluptatibus eligendi assumenda culpa odio maxime veritatis aperiam.
-        Cupiditate, enim, provident eaque officiis iusto eligendi, modi unde
-        voluptatum officia asperiores incidunt.
-      </p>
-      <h1>{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>add 1</button>
+      {toggle && (
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+          voluptatibus eligendi assumenda culpa odio maxime veritatis aperiam.
+          Cupiditate, enim, provident eaque officiis iusto eligendi, modi unde
+          voluptatum officia asperiores incidunt.
+        </p>
+      )}
+      <button onClick={() => setToggle(!toggle)}>add 1</button>
     </>
   );
 }
