455b17854241fdade542d298c4b93abde9940e1e
GS Kariyapperuma <89676555+GayathmiKariyapperuma@users.noreply.github.com>
2022-01-14 18:13:28 +0530

    use useeffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Home(props) {
   const [toggle, setToggle] = useState(false);
+
+  //componentDidMount
+  useEffect(() => {
+    console.log("Component Mounted");
+  }, []);
 
   return (
     <>
